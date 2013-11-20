29cd099579a69128b196748c282f6197dfd111ba
Matt Creager <mattcreager@goinstant.com>
2013-11-20 13:04:39 -0800

    stuff

---
+++ public/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+/* global angular */
+
+'use strict';
+
+angular.module('govote')
+  .controller('MainCtrl', function() {
+    console.log('We\'re up!');
+  });
---
+++ public/scripts/filters/order_ideas_by.js
@@ -0,0 +1,14 @@
+/* global angular, _ */
+
+angular.module('govote')
+  .filter('orderIdeasBy', function() {
+    'use strict';
+
+    return function(input, attribute) {
+      if (!_.isObject(input)) {
+        return;
+      }
+
+      return _.sortBy(input, attribute).reverse();
+    };
+  });
